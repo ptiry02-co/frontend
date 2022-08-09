5ab76ec6206c3d3793b49346ad8c8c5b303086e0
Paul Tiry <tirypaul@gmail.com>
2022-08-09 14:32:43 +0200

    fixed getPlans function

--- src/api/plans.js
+++ src/api/plans.js
@@ -1,7 +1,5 @@
 import { app } from './axios.config'
 
-const token = localStorage.getItem('authToken')
+export const getPlans = token => app.get('/plans', { headers: { authorization: `Bearer ${token}` } })
 
-export const getPlans = () => app.get('/plans', { headers: { authorization: `Bearer ${token}` } })
-
-export const newPlan = data => app.post('/plans', data, { headers: { authorization: `Bearer ${token}` } })
+export const newPlan = (data, token) => app.post('/plans', data, { headers: { authorization: `Bearer ${token}` } })
--- src/hooks/usePlans.js
+++ src/hooks/usePlans.js
@@ -4,16 +4,18 @@ import { getPlans, newPlan } from '../api/plans'
 const usePlans = () => {
   const [plansData, setPlansData] = useState({})
   const fetchPlans = async () => {
+    const token = localStorage.getItem('authToken')
     try {
-      const res = await getPlans()
+      const res = await getPlans(token)
       setPlansData(res.data)
     } catch (error) {
       console.log('Error fetching Plans: ', error)
     }
   }
   const addPlan = async data => {
+    const token = localStorage.getItem('authToken')
     try {
-      const res = await newPlan(data)
+      const res = await newPlan(data, token)
       setPlansData(res.data)
     } catch (error) {
       console.log('Error creating plan: ', error)
