ed7f04f82e736f8bb0d2b1cfb4db5154c5626367
Paul Tiry <tirypaul@gmail.com>
2022-08-11 22:57:45 +0200

    made app responsive

--- src/components/helpers/Box.jsx
+++ src/components/helpers/Box.jsx
@@ -9,13 +9,12 @@ export default Box
 const Container = styled.div`
   position: ${({ isModal }) => (isModal ? 'fixed' : 'relative')};
   height: ${({ isModal }) => (isModal ? 'auto' : '100%')};
-  width: ${({ isModal }) => (isModal ? 'auto' : '100%')};
-  margin: 0 5%;
+  width: ${({ isModal }) => (isModal ? '95%' : '100%')};
   align-self: center;
   text-align: center;
   top: ${({ isModal }) => (isModal ? '10%' : 'auto')};
-  padding: 30px 7%;
-  max-width: ${({ isModal }) => (isModal ? '40%' : '100%')};
+  padding: 30px 5px;
+  max-width: ${({ isModal }) => (isModal ? 'auto' : '100%')};
   border: 1px solid black;
   border-radius: 30px;
   min-width: 200px;
@@ -26,6 +25,24 @@ const Container = styled.div`
   z-index: ${({ isModal }) => (isModal ? 1 : 0)};
   background-color: ${({ isModal }) => (isModal ? 'white' : '#D9D9D9')};
   box-shadow: 0 0 5px 5px darkgray;
+  ${({ isModal }) =>
+    isModal &&
+    `@media (min-width: 460px) {
+      width: 65%;
+    }
+    @media (min-width: 650px) {
+      width: 50%;
+    }
+    @media (min-width: 835px) {
+      width: 45%;
+    }
+    @media (min-width: 1000px) {
+      width: 40%;
+      padding: 30px 3%;
+    }
+    @media (min-width: 1230px) {
+      width: 35%;
+    }`}
   > * {
     align-self: center;
   }
