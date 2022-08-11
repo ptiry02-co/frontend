3f4b29b439c84c1b9e90dd48f3b0bd47c95953c1
Paul Tiry <tirypaul@gmail.com>
2022-08-11 22:57:45 +0200

    made app responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,12 +12,25 @@ const HomePage = () => {
 export default HomePage
 
 const Wrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   img {
+    width: 80%;
     border-radius: 50%;
     box-shadow: 0 0 5px 5px darkgray;
+    @media (min-width: 365px) {
+      width: 70%;
+    }
+    @media (min-width: 570px) {
+      width: 60%;
+    }
+    @media (min-width: 880px) {
+      width: 50%;
+    }
+    @media (min-width: 1230px) {
+      width: 35%;
+    }
   }
 `
